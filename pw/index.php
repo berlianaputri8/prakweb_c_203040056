7db480bb524e822e864dac2bf6ded422594b9858
berlianaputri8 <berlianaap08@gmail.com>
2022-10-03 10:20:27 +0700

    background

--- pw/index.php
+++ pw/index.php
@@ -15,6 +15,7 @@ $buku = query("SELECT * FROM buku");
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Daftar Buku</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="../pw/style.css"
   </head>
 
 <body class="luar">
