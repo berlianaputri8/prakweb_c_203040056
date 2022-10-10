3000c10daa43a33739630fda8a0b389f3a9381eb
berlianaputri8 <75973686+berlianaputri8@users.noreply.github.com>
2022-10-10 19:33:28 +0700

    latihan mvc Database Wrapper

---
+++ mvc/app/config/config.php
@@ -0,0 +1,9 @@
+<?php 
+
+define('BASEURL', 'http://localhost/prakweb_C_203040074/mvc/code/public/');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
--- mvc/app/views/templates/header.php
+++ mvc/app/views/templates/header.php
@@ -2,30 +2,24 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Halaman <?= $data ['judul']; ?></title>
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"> -->
-    <link rel="stylesheet" href="<?= BASEURL; ?>../public/css/bootstrap.css">
+    <title>Halaman <?= $data['judul']; ?></title>
+    <link rel="stylesheet" href="<?= BASEURL; ?>/css/bootstrap.css">
+    <link rel="stylesheet" href="<?= BASEURL; ?>/css/style.css">
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container">
-    <a class="navbar-brand" href="<?= BASEURL; ?>">PHP MVC</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="<?= BASEURL; ?>">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?= BASEURL; ?>/mahasiswa">Mahasiswa</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?= BASEURL; ?>/about">About</a>
-        </li>
-      </ul>
-    </div>
+      <a class="navbar-brand" href="<?= BASEURL; ?>">PHP MVC</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-item nav-link active" href="<?= BASEURL; ?>">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="<?= BASEURL; ?>/mahasiswa">Mahasiswa</a>
+          <a class="nav-item nav-link" href="<?= BASEURL; ?>/about">About</a>
+        </div>
+      </div>
   </div>
 </nav>
